3395809525c0abed6510936a3c9c942c0acfd1c7
Milionj <sergeweb52@gmail.com>
2025-03-29 19:58:06 +0100

    finalisation inscription

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import './Signup.css';
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 function Signup() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h2 className="text-2xl font-bold">Page Signup</h2>
+    <div className="signup-wrapper">
+      <div className="signup-container">
+        <div className="signup-left" />
+
+        <div className="signup-right">
+          <h2 className="signup-title">Inscription</h2>
+          <div className="signup-form-box">
+            <form>
+              <div className="name-fields">
+                <div>
+                  <label>Nom :</label>
+                  <input type="text" placeholder="Entrez votre nom..." />
+                </div>
+                <div>
+                  <label>Prénom :</label>
+                  <input type="text" placeholder="Entrez votre prénom..." />
+                </div>
+              </div>
+
+              <label>Email :</label>
+              <input type="email" placeholder="Entrez votre email..." />
+
+              <div className="signup-gender">
+                <label>Genre :</label>
+                <label><input type="radio" name="genre" /> Homme</label>
+                <label><input type="radio" name="genre" /> Femme</label>
+              </div>
+
+              <label>Mot de passe :</label>
+              <input type="password" placeholder="Entrez votre mot de passe..." />
+
+              <label>Vérification de mot de passe :</label>
+              <input type="password" placeholder="Entrez votre mot de passe..." />
+
+              <button type="submit">Valider</button>
+            </form>
+
+            <div className="signup-footer">
+              Déjà inscrit ? <Link to="/login">Connectez-vous</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
